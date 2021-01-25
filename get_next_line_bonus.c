e12fa4f29d6638b20e8f87a1dd3aa0514a23be42
user <user@userui-MacBookPro.local>
2021-01-26 00:50:24 +0900

    Cursus first passed source

---
+++ get_next_line_bonus.c
@@ -0,0 +1,75 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ulee <ulee@student.42seoul.kr>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/01/20 21:05:34 by ulee              #+#    #+#             */
+/*   Updated: 2021/01/21 01:27:13 by ulee             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line_bonus.h"
+
+int		isnewline(char **backup, char **line)
+{
+	char	*newline;
+	char	*temp;
+
+	if (!(newline = ft_strchr(*backup, '\n')))
+		return (0);
+	*newline = '\0';
+	if ((!(*line = ft_strdup(*backup))))
+		return (-1);
+	if (!(temp = ft_strdup(++newline)))
+		return (-1);
+	free(*backup);
+	*backup = temp;
+	return (1);
+}
+
+int		backup_clean(char **backup, char **line, int read_size)
+{
+	if (read_size < 0)
+		return (-1);
+	if (*backup && (isnewline(backup, line)))
+		return (1);
+	else if (*backup)
+	{
+		if (!(*line = ft_strdup(*backup)))
+			return (-1);
+		free(*backup);
+		*backup = NULL;
+		return (0);
+	}
+	if (!(*line = ft_strdup("")))
+		return (-1);
+	return (0);
+}
+
+int		get_next_line(int fd, char **line)
+{
+	static char		*backup[OPEN_MAX];
+	char			buf[BUFFER_SIZE + 1];
+	int				read_size;
+	char			*temp;
+	int				error;
+
+	if ((fd < 0) || (line == 0) || (BUFFER_SIZE < 1))
+		return (-1);
+	while ((read_size = read(fd, buf, BUFFER_SIZE)) > 0)
+	{
+		buf[read_size] = '\0';
+		temp = backup[fd];
+		if (!(backup[fd] = ft_strjoin(backup[fd], buf)))
+			return (-1);
+		if (temp)
+			free(temp);
+		if ((error = isnewline(&backup[fd], line)))
+			return (1);
+		else if (error == -1)
+			return (-1);
+	}
+	return (backup_clean(&backup[fd], line, read_size));
+}
---
+++ get_next_line_utils.c
@@ -0,0 +1,92 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ulee <ulee@student.42seoul.kr>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/01/20 21:05:12 by ulee              #+#    #+#             */
+/*   Updated: 2021/01/20 21:05:16 by ulee             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+size_t	ft_strlen(const char *str)
+{
+	size_t len;
+
+	len = 0;
+	while (*str++)
+		len++;
+	return (len);
+}
+
+void	*ft_memcpy(void *d, const void *s, size_t n)
+{
+	size_t			i;
+	unsigned char	*d2;
+	unsigned char	*s2;
+
+	if (d == 0 && s == 0)
+		return (d);
+	d2 = (unsigned char *)d;
+	s2 = (unsigned char *)s;
+	i = 1;
+	while (i <= n)
+	{
+		*d2++ = *s2++;
+		i++;
+	}
+	return (d);
+}
+
+char	*ft_strchr(const char *str, int c)
+{
+	if (c == 0)
+		return ((char *)str + ft_strlen(str));
+	while (*str)
+	{
+		if (*str == c)
+			return ((char *)str);
+		str++;
+	}
+	return (0);
+}
+
+char	*ft_strdup(const char *s)
+{
+	char	*result;
+	int		len;
+
+	len = ft_strlen(s);
+	if (!(result = malloc(sizeof(char) * (len + 1))))
+		return (0);
+	ft_memcpy(result, s, len);
+	result[len] = 0;
+	return (result);
+}
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*result;
+	char	*result2;
+	int		s1_len;
+	int		s2_len;
+
+	if (s1 == NULL && s2 == NULL)
+		return (NULL);
+	else if (s1 == NULL || s2 == NULL)
+		return (s1 == NULL ? ft_strdup(s2) : ft_strdup(s1));
+	s1_len = ft_strlen(s1);
+	s2_len = ft_strlen(s2);
+	if (!(result = (char *)malloc(sizeof(char) * (s1_len + s2_len + 1))))
+		return (0);
+	result2 = result;
+	while (*s1)
+		*result2++ = *s1++;
+	while (*s2)
+		*result2++ = *s2++;
+	*result2 = 0;
+	return (result);
+}

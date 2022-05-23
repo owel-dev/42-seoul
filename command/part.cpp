43f37394cae1474c6a0723cf61cc992ebfbb7bd1
3dots3craters <3dots3craters@gmail.com>
2022-05-23 17:40:17 +0900

    Fix: fix problems about PART
    - 채널 안에 사람이 한 명도 없는 경우 해당 채널을 서버의 채널목록에서 삭제하는 기능 추가
    - 채널 어드민이 나간 경우, 다음 사람을 어드민으로 지정하는 기능 추가
    - 채팅방 유저 목록 업데이트 기능 추가

--- command/part.cpp
+++ command/part.cpp
@@ -6,13 +6,29 @@ void Server::part(std::vector<string> command, struct kevent event)
     // 해당 채널의 유저 목록을 돌면서 메시지 전송
     string channelName = command[1];
     string message = command[2];
-    map<int, User>::iterator it = m_channelList[channelName].m_userList.begin();
+    Channel &channel = m_channelList[channelName];
+    map<int, User>::iterator it = channel.m_userList.begin();
     User &sender = m_userList[event.ident];
     string msg = channelName + " " + message;
-    for (;it != m_channelList[channelName].m_userList.end(); ++it)
+    for (;it != channel.m_userList.end(); ++it)
     {
         User &receiver = it->second;
         m_userList[receiver.getFd()].setWriteBuffer(prefixMessage(sender.getNick(), sender.getUserInfo(), sender.getHostName(), "part", msg));
     }
-    m_channelList[channelName].m_userList.erase(event.ident);
+    channel.m_userList.erase(event.ident);
+    // 채널 안에 사람이 한 명도 없는 경우 해당 채널 지우기
+    if (channel.m_userList.size() == 0) {
+        m_channelList.erase(channelName);
+    }
+    // 채널 안에 사람이 있으면서 방금 나간 사람이 어드민인 경우, 다음 사람으로 어드민 바꾸기
+    else if (channel.m_admin == event.ident) {
+        int firstFd = (channel.m_userList.begin())->second.getFd();
+        channel.setAdmin(firstFd);
+    }
+    // 채팅방 유저 목록 업데이트
+    for (it = channel.m_userList.begin(); it != channel.m_userList.end(); ++it)
+    {
+        User &receiver = it->second;
+        m_userList[receiver.getFd()].setWriteBuffer(serverMessage(353, receiver.getNick(), receiver.getUserInfo(), channelName, channel.getUserList(receiver.getFd())));
+    }
 }

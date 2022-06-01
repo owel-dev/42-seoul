b722e3b3e7e7ada02603fb3bf36f6f1a17930f92
samin <samin@sutdent.42seoul.kr>
2022-06-01 09:27:28 +0900

    Feat: Add nick error handling
    -허용불가문자 , 공백 에러 처리

--- commands/nick.cpp
+++ commands/nick.cpp
@@ -7,6 +7,15 @@ void nick(User &user, string newNickName, int fd){
     string hostName = user.getHostName(fd);
     string fullMessage = prefixMessage(nickName, loginName, hostName, "NICK", newNickName);
 
+    if (newNickName == ""){
+        user.setWriteBuffer(fd, serverMessage(ERR_NONICKNAMEGIVEN, nickName, "", "", "No nickname given"));
+        return;
+    }
+    if (validNickName(newNickName) == false)
+    {
+        user.setWriteBuffer(fd, serverMessage(ERR_ERRONEUSNICKNAME, nickName, newNickName, "", "Erroneous nickname"));
+        return;
+    }
     if(user.isExistUser(newNickName))
     {
         user.setWriteBuffer(fd, serverMessage(ERR_NICKNAMEINUSE, nickName, "", "", "Nickname is already in use"));
@@ -17,4 +26,15 @@ void nick(User &user, string newNickName, int fd){
     
     if (user.isLogin(fd))
         user.setBroadCastMessageToAllUser(fullMessage);
+}
+
+bool validNickName(string newNickName)
+{
+    size_t len = newNickName.length();
+    for (size_t i = 0; i < len; i++)
+    {
+        if (!(newNickName[i]  >= 65 && newNickName[i] <= 126) && !(newNickName[i]  >= 48 && newNickName[i] <= 57))
+            return (false);
+    }
+    return (true);
 }

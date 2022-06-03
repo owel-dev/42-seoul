3d9a329d8e1a150f0cc7ffeac6ec742adde94ff5
3dots3craters <3dots3craters@gmail.com>
2022-06-03 13:06:49 +0900

    Remove: remove comment

--- define.hpp
+++ define.hpp
@@ -9,21 +9,19 @@
 #define RPL_NAMREPLY 353
 #define RPL_ENDOFNAMES 366 
 
-///////////// error code
-
-#define ERR_NOSUCHNICK 401      // 닉네임을 아무도 안쓰고 있을 때. // 
-#define ERR_NEEDMOREPARAMS 461  // 커맨드 인자갯수 부족할때. // 구현해야 함
-#define ERR_PASSWDMISMATCH 464  // 비밀번호 안넣었거나 틀렸을때. // 구현해야 함
-#define ERR_NONICKNAMEGIVEN 431 // 닉 다음 인자를 안줬을경우. // 구현해야 함
-#define ERR_ERRONEUSNICKNAME 432 // 닉에 이상한거 들어온경우 // 구현해야 함
-#define ERR_NICKNAMEINUSE 433    // 닉네임 중복 // o
-#define ERR_NOTREGISTERED 451    // 등록 안된 상태일때 // 구현해야 함
-#define ERR_BADCHANMASK 476      // 채널에 #이 안붙었을때. // 구현해야 함
-#define ERR_NOSUCHCHANNEL 403 // 구현해야 함
-#define ERR_NORECIPIENT 411 // 구현해야 함
-#define ERR_NOTEXTTOSEND 412 // 구현해야 함
-#define ERR_UNKNOWNCOMMAND 421 // 구현해야 함
-#define ERR_NOPRIVILEGES 481 // o
+#define ERR_NOSUCHNICK 401      
+#define ERR_NEEDMOREPARAMS 461  
+#define ERR_PASSWDMISMATCH 464  
+#define ERR_NONICKNAMEGIVEN 431 
+#define ERR_ERRONEUSNICKNAME 432 
+#define ERR_NICKNAMEINUSE 433    
+#define ERR_NOTREGISTERED 451    
+#define ERR_BADCHANMASK 476      
+#define ERR_NOSUCHCHANNEL 403
+#define ERR_NORECIPIENT 411
+#define ERR_NOTEXTTOSEND 412
+#define ERR_UNKNOWNCOMMAND 421
+#define ERR_NOPRIVILEGES 481
 #define ERR_ALREADYREGISTRED 462
 #define ERR_NOTONCHANNEL 442
 #define ERR_CANNOTSENDTOCHAN 404

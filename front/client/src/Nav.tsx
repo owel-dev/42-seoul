ed630cda89472c2d347854e283fea0f5ccafa796
jeonhyun <jeonhyun@student.42seoul.kr>
2022-07-06 14:25:57 +0900

    [Feat - Front] 랭킹 페이지 작성 #7

--- front/client/src/Nav.tsx
+++ front/client/src/Nav.tsx
@@ -1,12 +1,17 @@
+import { Link } from 'react-router-dom';
 import './Nav.css';
 
 function Nav(){
     return (
         <div>
             <header className="nav">
-            <button className='nav_menu'>logo</button>
+            <Link to="home" className='link'>
+                <button className='nav_menu'>logo</button>
+            </Link>
             <span className='nav_menu' id="blank"></span>
-            <button className='nav_menu'>rank</button>
+            <Link to="ranking" className='link'>
+                <button className='nav_menu'>rank</button>
+            </Link>
             <button className='nav_menu'>mypage</button>
             <button className='nav_menu'>logout</button>
         </header>

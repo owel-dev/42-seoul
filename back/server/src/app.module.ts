20037e3e178194c5ce3a5a57c06067e3c65c1bd9
yongwkim <kyw9404@gmail.com>
2022-07-07 15:53:49 +0900

    [Feat - Back] Ranking Get api 구현

--- back/server/src/app.module.ts
+++ back/server/src/app.module.ts
@@ -1,9 +1,27 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { StatsModule } from './stats/stats.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Stat } from './stats/entities/stat.entity';
+import { UsersModule } from './users/users.module';
+import { RankingModule } from './ranking/ranking.module';
 
 @Module({
-  imports: [],
+  imports: [StatsModule,
+	TypeOrmModule.forRoot({
+		"type": "postgres",
+		"host": "localhost",
+		"port": 5432,
+		"username": "yongwkim",
+		"password": "test",
+		"database": "test",
+		"entities": ["dist/**/*.entity{.ts,.js}"],
+		"synchronize": true
+		}),
+	UsersModule,
+	RankingModule,
+	],
   controllers: [AppController],
   providers: [AppService],
 })

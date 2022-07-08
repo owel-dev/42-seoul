45a36233ed64d9c44f8ec0eb0189f0d5bd120761
jeonhyun <jeonhyun@student.42seoul.kr>
2022-07-08 15:25:28 +0900

    [fix - Front/Back] 마이페이지 컴포넌트들 get요청으로 수정 #30

--- front/client/src/components/mypage/FriendTable.tsx
+++ front/client/src/components/mypage/FriendTable.tsx
@@ -1,34 +1,56 @@
+import {useState, useEffect} from 'react';
+import axios from 'axios';
 import 'styles/mypage/FriendTable.css'
+import {friendList} from 'types/MyPageTypes'
 
-const dummy = {
-    friendList:
-        [
-            {
-                nickName : "kim",
-                status : "online"
-            },
-            {
-                nickName : "lee",
-                status : "offline"
-            }, 
-            {
-                nickName : "park",
-                status : "online"
-            }, 
-            {
-                nickName : "choi",
-                status : "ingame"
-            }
-]
-};
+// const dummy = {
+//     friendList:
+//         [
+//             {
+//                 nickName : "kim",
+//                 status : "online"
+//             },
+//             {
+//                 nickName : "lee",
+//                 status : "offline"
+//             }, 
+//             {
+//                 nickName : "park",
+//                 status : "online"
+//             }, 
+//             {
+//                 nickName : "choi",
+//                 status : "ingame"
+//             }
+// ]
+// };
 
+const id = "hello";
 
 function FriendTable() {
+    const [List, setList] = useState<friendList | null>(null);
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const getAPI = await axios.get(
+                    'http://localhost:3000/users/' + {id} + '/friends', {
+                    headers: {
+                        'Content-Type': 'application/json'
+                    }
+                });
+                setList(getAPI.data);
+            } catch (e) {
+                console.log(e);
+            }
+        };
+        fetchData();
+    }, []);
+
     return (
         <div className='friend-table'>
-            {dummy.friendList.map((element) => {
+            {List?.friendList.map((element, index) => {
                 return (
-                    <div className='friend-row'>
+                    <div className='friend-row' key={index}>
                         <span>{element.nickName} </span>
                         <span>{element.status}</span>
                     </div>
--- front/client/src/components/mypage/MatchTable.tsx
+++ front/client/src/components/mypage/MatchTable.tsx
@@ -1,35 +1,59 @@
+import {useState, useEffect} from 'react';
+import axios from 'axios';
 import 'styles/mypage/MatchTable.css'
+import {matchList} from 'types/MyPageTypes'
 
-const dummy = {
-    matchList:
-        [
-            {
-                player1 : "hello",
-                player2 : "bye",
-                playerScore1 : 7,
-                playerScore2 : 4
-            },
-            {
-                player1 : "hello",
-                player2 : "zzz",
-                playerScore1 : 6,
-                playerScore2 : 5
-            },
-            {
-                player1 : "hello",
-                player2 : "qapla",
-                playerScore1 : 2,
-                playerScore2 : 9
-            }
-        ]
-};
+// const dummy = {
+//     matchList:
+//         [
+//             {
+//                 player1 : "hello",
+//                 player2 : "bye",
+//                 playerScore1 : 7,
+//                 playerScore2 : 4
+//             },
+//             {
+//                 player1 : "hello",
+//                 player2 : "zzz",
+//                 playerScore1 : 6,
+//                 playerScore2 : 5
+//             },
+//             {
+//                 player1 : "hello",
+//                 player2 : "qapla",
+//                 playerScore1 : 2,
+//                 playerScore2 : 9
+//             }
+//         ]
+// };
+
+
+const id : string = "hello";//임시값
 
 function MatchTable(){
+    const [List, setList] = useState<matchList | null>(null);
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const getAPI = await axios.get(
+                    'http://localhost:3000/users/' + {id} + '/match', {
+                    headers: {
+                        'Content-Type': 'application/json'
+                    }
+                });
+                setList(getAPI.data);
+            } catch (e) {
+                console.log(e);
+            }
+        };
+        fetchData();
+    }, []);
+
     return (
         <div className='match-table'>
-            {dummy.matchList.map((element) => {
+            {List?.matchList.map((element, index) => {
                 return (
-                    <div className='match-row'>
+                    <div className='match-row' key={index}>
                         <span>{element.player1} </span>
                         <span>{element.playerScore1}</span>
                         <span> vs </span>

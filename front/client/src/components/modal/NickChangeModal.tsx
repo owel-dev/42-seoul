f7d01fd8270dc2225c0f472db69edf2f0fbba0be
jeonhyun <jeonhyun@student.42seoul.kr>
2022-07-12 17:07:34 +0900

    [Fix - front] 닉네임 변경 POST로 고치기

--- front/client/src/components/modal/NickChangeModal.tsx
+++ front/client/src/components/modal/NickChangeModal.tsx
@@ -1,12 +1,35 @@
 import { nickChangeModalState } from 'utils/recoil/modalState';
 import { useRecoilState } from 'recoil';
 import 'styles/modal/Modal.css';
+import { useState } from 'react';
+import axios from 'axios';
+import { DUMMY_SERVER, DUMMY_USER } from 'utils/dummy';
 
 function NickChangeModal() {
   const [status, setStatus] = useRecoilState(nickChangeModalState);
+  const [inputValue, setInputValue] = useState('');
   const CloseModal = () => {
     setStatus(false);
   };
+  function PostNickName() {
+    const fetchData = async () => {
+      try {
+        const getAPI = await axios.patch(
+          DUMMY_SERVER + 'users/' + DUMMY_USER.intraId,
+          {
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            nickName: inputValue,
+          }
+        );
+        console.log(getAPI.data);
+      } catch (e) {
+        console.log(e);
+      }
+    };
+    fetchData();
+  }
   if (status === false) return <div></div>;
   else
     return (
@@ -15,11 +38,14 @@ function NickChangeModal() {
         <div className='modal-content'>
           <div>
             <span>nickname </span>
-            <input placeholder={'닉네임 입력'}></input>
+            <input
+              placeholder={'닉네임 입력'}
+              onChange={(e) => setInputValue(e.target.value)}
+            ></input>
           </div>
         </div>
         <div className='modal-select'>
-          <button>change</button>
+          <button onClick={PostNickName}>change</button>
           <button onClick={CloseModal}>close</button>
         </div>
       </div>

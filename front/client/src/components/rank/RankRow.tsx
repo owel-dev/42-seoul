596adcec18e8601c61ed9886aa85d718209d4a65
jeonhyun <jeonhyun@student.42seoul.kr>
2022-07-21 16:45:45 +0900

    [Fix - Front] 랭킹 타이틀을 별도 컴포넌트로 분리

--- front/client/src/components/rank/RankRow.tsx
+++ front/client/src/components/rank/RankRow.tsx
@@ -4,8 +4,8 @@ function RankRow(props: any) {
   return (
     <div className={props.type}>
       <span className='rank-cell'>{props.rank}</span>
-      <Link to={`/users/${props.nickName}/mypage`}>
-        <span className='rank-cell'>{props.nickName}</span>
+      <Link className='rank-cell' to={`/users/${props.nickName}/mypage`}>
+        <span>{props.nickName}</span>
       </Link>
       <span className='rank-cell'>{props.win}</span>
       <span className='rank-cell'>{props.lose}</span>
---
+++ front/client/src/components/rank/RankTitleRow.tsx
@@ -0,0 +1,13 @@
+function RankTitleRow() {
+  return (
+    <div className='rank-row-title'>
+      <span className='rank-cell'>ranking</span>
+      <span className='rank-cell'>nickname</span>
+      <span className='rank-cell'>win</span>
+      <span className='rank-cell'>lose</span>
+      <span className='rank-cell'>winrate</span>
+    </div>
+  );
+}
+
+export default RankTitleRow;

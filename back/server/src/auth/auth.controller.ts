5eb8f56d47c7c189b5ff004fdde2017af00878a5
yongwkim <71074220+benelus94@users.noreply.github.com>
2022-07-21 15:07:32 +0900

    [Fix - Back] Redirection 에러 수정

--- back/server/src/auth/auth.controller.ts
+++ back/server/src/auth/auth.controller.ts
@@ -12,8 +12,7 @@ export class AuthController {
 		await this.authService.saveAccessToken(res, code);
 	}
  
-	@Redirect('https://api.intra.42.fr/oauth/authorize?client_id=10fd003cd72e573d39cefc1302e9a5c3a9722ad06f7bffe91bf3b3587ace5036&redirect_uri=http%3A%2F%2F10.19.236.57%3A3000%2Foauth%2Flogin&response_type=code'
-	+ 'scope=public', 301)
+	@Redirect('https://api.intra.42.fr/oauth/authorize?client_id=10fd003cd72e573d39cefc1302e9a5c3a9722ad06f7bffe91bf3b3587ace5036&redirect_uri=http%3A%2F%2F10.19.236.57%3A3000%2Foauth%2Flogin&response_type=code', 301)
 	@Get('42')
 	getOauthPage() {
 		console.log("authorization/42");

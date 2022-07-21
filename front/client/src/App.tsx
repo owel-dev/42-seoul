8f4f7f7329fb56c6987ecef5c35d3aae79ffaa8b
jeonhyun <jeonhyun@student.42seoul.kr>
2022-07-21 15:37:38 +0900

    Fix - Front Route 경로 수정

--- front/client/src/App.tsx
+++ front/client/src/App.tsx
@@ -22,7 +22,7 @@ function App() {
                 <Route path='/ranking' element={<Rank />} />
                 <Route path='/users/:nickName/mypage' element={<UserPage />} />
                 <Route path='/' element={<Lobby />} />
-                <Route path='/game/:channelId' element={<Game />} />
+                <Route path='/channel/:channelId' element={<Game />} />
               </Routes>
             </Layout>
             <ModalProvider />

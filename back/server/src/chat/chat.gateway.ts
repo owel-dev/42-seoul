76ff1d172d09a751c0833885c14350195e3f5be6
yongwkim <kyw9404@gmail.com>
2022-07-25 14:54:40 +0900

    [Feat - Back] 채팅 모듈 구현

---
+++ back/server/src/chat/chat.gateway.ts
@@ -0,0 +1,74 @@
+import { Query, UseGuards } from '@nestjs/common';
+import {
+  ConnectedSocket,
+  MessageBody,
+  OnGatewayConnection,
+  OnGatewayDisconnect,
+  OnGatewayInit,
+  SubscribeMessage,
+  WebSocketGateway,
+  WebSocketServer,
+} from '@nestjs/websockets';
+import { Server, Socket } from 'socket.io';
+import { Token } from 'src/auth/auth.decorator';
+import { AuthGuard } from 'src/auth/auth.guard';
+import { ChatService } from './chat.service';
+
+@WebSocketGateway({
+  cors: {
+    origin: '*',
+  },
+})
+export class ChatGateway
+  implements OnGatewayConnection, OnGatewayDisconnect, OnGatewayInit
+{
+  constructor(private readonly chatService: ChatService) {}
+
+  @WebSocketServer()
+  server: Server;
+
+  afterInit() {}
+
+  handleConnection(socket: Socket) {
+    this.chatService.handleConnection(socket, this.server);
+  }
+
+  handleDisconnect(socket: Socket) {
+    this.chatService.handleDisconnect(socket, this.server);
+  }
+
+  @SubscribeMessage('create-channel')
+  createChannel(@ConnectedSocket() client: Socket, @MessageBody() data: any) {
+    return this.chatService.createChannel(client, this.server);
+  }
+
+  @SubscribeMessage('join-channel')
+  joinChannel(@ConnectedSocket() client: Socket, @MessageBody() data: any) {
+    return this.chatService.joinChannel(client, data, this.server);
+  }
+
+  @SubscribeMessage('direct-message')
+  directMessage(@ConnectedSocket() client: Socket, @MessageBody() data: any) {
+    return this.chatService.sendDirectMessage(client, data, this.server);
+  }
+
+  @SubscribeMessage('message')
+  channelMessage(@ConnectedSocket() client: Socket, @MessageBody() data: any) {
+    return this.chatService.sendChannelMessage(client, data, this.server);
+  }
+
+  @SubscribeMessage('user-list')
+  getUserList(@ConnectedSocket() client: Socket) {
+    return this.chatService.sendUserList(client, this.server);
+  }
+
+  @SubscribeMessage('mute')
+  muteUser(@ConnectedSocket() client: Socket, @MessageBody() data: any) {
+    return this.chatService.muteUser(client, data, this.server);
+  }
+
+  @SubscribeMessage('unmute')
+  unmuteUser(@ConnectedSocket() client: Socket, @MessageBody() data: any) {
+    return this.chatService.unmuteUser(client, data, this.server);
+  }
+}
---
+++ back/server/src/chat/chat.ts
@@ -0,0 +1,19 @@
+import { Socket } from 'socket.io';
+import { User } from 'src/users/entities/user.entity';
+
+export class ChatUser {
+  intraId: string;
+  socket: Socket;
+  curChannel: string;
+
+  constructor(intraId: string, socket: Socket) {
+    this.intraId = intraId;
+    this.socket = socket;
+    this.curChannel = '';
+  }
+}
+
+export class Channel {
+  players: string[] = [];
+  muteList: string[] = [];
+}

037cca757d6b72e326c50963cfcbd12ab801945c
yongwkim <kyw9404@gmail.com>
2022-08-05 16:02:41 +0900

    [Refact - Back] 채팅 모듈 리팩토링 및 버그 수정

--- back/server/src/auth/auth.controller.ts
+++ back/server/src/auth/auth.controller.ts
@@ -1,40 +1,52 @@
-import { Body, Controller, Get, Param, Patch, Post, Query, Redirect, Req, Res } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+  Redirect,
+  Req,
+  Res,
+} from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { Response, Request } from 'express';
 import { ResUserNavi } from 'src/users/dto/res-user-navi.dto';
 import { AuthService } from './auth.service';
 
 @Controller('oauth')
 export class AuthController {
-  constructor(private readonly authService: AuthService) {}
+  constructor(
+    private readonly authService: AuthService,
+    private config: ConfigService,
+  ) {}
 
   @Get('test')
-  test(@Req() req: Request)
-  {
+  test(@Req() req: Request) {
     console.log(req.cookies);
   }
 
-	@Get('login')
-	async saveAccessToken(@Res() res: Response, @Query('code') code: string) {
-    console.log("로그인");
-    (await this.authService.saveAccessToken(res, code));
+  @Get('login')
+  async saveAccessToken(@Res() res: Response, @Query('code') code: string) {
+    await this.authService.saveAccessToken(res, code);
+  }
 
-    console.log("객체 확인");    
-	}
+  @Patch('logout')
+  async logout() {
+    //클라이언트 측에서 토큰 삭제하고 소켓 연결 끊어주기만 하면 될듯.
+    return;
+  }
 
-	@Patch('logout')
-	async logout()
-	{
-		//클라이언트 측에서 토큰 삭제하고 소켓 연결 끊어주기만 하면 될듯.
-		return ;
-	}
-
-	// @Redirect('https://api.intra.42.fr/oauth/authorize?client_id=10fd003cd72e573d39cefc1302e9a5c3a9722ad06f7bffe91bf3b3587ace5036&redirect_uri=http%3A%2F%2F10.19.236.57%3A3000%2Foauth%2Flogin&response_type=code', 301)
-  @Redirect('https://api.intra.42.fr/oauth/authorize?client_id=ed7c75ff5c1b092be1782335c06bed873fe064ef1e9eb82256f6ec202c8c1047&redirect_uri=http%3A%2F%2F10.19.247.186%3A3000%2Foauth%2Flogin&response_type=code', 302)
+  @Redirect(
+    'https://api.intra.42.fr/oauth/authorize?client_id=10fd003cd72e573d39cefc1302e9a5c3a9722ad06f7bffe91bf3b3587ace5036&redirect_uri=http%3A%2F%2F10.19.236.57%3A3000%2Foauth%2Flogin&response_type=code',
+    302,
+  )
   @Get('42')
-	getOauthPage() {
-		console.log("authorization/42");
-		return;
-	}
+  getOauthPage() {
+    // console.log('authorization/42');
+    return;
+  }
 
   @Post('sendEmail')
   async sendEmail(
--- back/server/src/auth/auth.service.ts
+++ back/server/src/auth/auth.service.ts
@@ -1,9 +1,9 @@
 import {
-	HttpException,
-	HttpStatus,
-	Injectable,
-	NotFoundException,
-	Res,
+  HttpException,
+  HttpStatus,
+  Injectable,
+  NotFoundException,
+  Res,
 } from '@nestjs/common';
 import axios from 'axios';
 import { Response } from 'express';
@@ -16,41 +16,29 @@ import { MailerService } from '@nestjs-modules/mailer';
 import { ResUserNavi } from 'src/users/dto/res-user-navi.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { AuthJwtService } from 'src/auth-jwt/auth-jwt.service';
-
+import { ConfigService } from '@nestjs/config';
 
 const hashedCodes = {};
 
 @Injectable()
 export class AuthService {
-	constructor(
-		@InjectRepository(User)
-		private userRepository: Repository<User>,
-		private mailerService: MailerService,
-		private authJwtService: AuthJwtService,
+  constructor(
+    @InjectRepository(User)
+    private userRepository: Repository<User>,
+    private mailerService: MailerService,
+    private config: ConfigService,
+    private authJwtService: AuthJwtService,
+  ) {}
 
-	) { }
-
-	async getAccessToken(code: string): Promise<string> {
-		console.log('code=', code);
-		// const payload = {
-		// 	grant_type: 'authorization_code',
-		// 	client_id:
-		// 		'10fd003cd72e573d39cefc1302e9a5c3a9722ad06f7bffe91bf3b3587ace5036',
-		// 	client_secret:
-		// 		'813cf74e92c49fd3c4afadf409bd6eb818fe985a11aefbfb4579bfcf0c032d96',
-		// 	redirect_uri: 'http://10.19.236.57:3000/oauth/login',
-		// 	code
-		// };
-
+  async getAccessToken(code: string): Promise<string> {
+    // console.log('code=', code);
     const payload = {
-			grant_type: 'authorization_code',
-			client_id:
-				'ed7c75ff5c1b092be1782335c06bed873fe064ef1e9eb82256f6ec202c8c1047',
-			client_secret:
-				'006cd6273030c6d3aa6dec3aeaccd4b720f920f6da54e1a30b43e70d5ae98d9a',
-        redirect_uri: 'http://10.19.247.186:3000/oauth/login',
-			code
-		};
+      grant_type: 'authorization_code',
+      client_id: this.config.get('INTRA_UID'),
+      client_secret: this.config.get('INTRA_SECRET'),
+      redirect_uri: this.config.get('REDIRECT_URI'),
+      code,
+    };
     let ret: string;
     await axios
       .post('https://api.intra.42.fr/oauth/token', JSON.stringify(payload), {
@@ -100,72 +88,66 @@ export class AuthService {
     return userData;
   }
 
-	async saveAccessToken(@Res() response: Response, code: string) {
-		console.log('saveAccessToken');
-		const newUser: CreateUserDto = await this.getUserData(code);
-		// console.log(newUser);
-		if (!newUser) {
-			throw new HttpException('Invalid User', HttpStatus.BAD_REQUEST);
-		}
-		let user = await this.userRepository.findOneBy({
-			intra_id: newUser.intraId
-		});
-		if (!user) {
-			let userEntity = new User();
+  async saveAccessToken(@Res() response: Response, code: string) {
+    console.log('saveAccessToken');
+    const newUser: CreateUserDto = await this.getUserData(code);
+    // console.log(newUser);
+    if (!newUser) {
+      throw new HttpException('Invalid User', HttpStatus.BAD_REQUEST);
+    }
+    let user = await this.userRepository.findOneBy({
+      intra_id: newUser.intraId,
+    });
+    if (!user) {
+      let userEntity = new User();
       userEntity = {
         intra_id: newUser.intraId,
         nickname: newUser.nickName,
         intra_email: newUser.intraEmail,
         avatar: newUser.avatar,
-        status: 'online',
+        status: 'offline',
         channel_id: '0',
         socket_id: null,
         stats: new Stat(),
         is_second_auth: false,
       };
-			user = await this.userRepository.save(userEntity);
-		}
+      user = await this.userRepository.save(userEntity);
+    }
     console.log(user);
-		
-		const jwtToken = this.login(user.intra_id);
-		response.cookie(
-			"accessToken", (await jwtToken).accessToken,
-			{
-        sameSite:'lax',
-				httpOnly: true,
-        path : '/',
-				maxAge: 24 * 60 * 60 * 1000
-			}
-		);
-		response.cookie(
-			"refreshToken", (await jwtToken).refreshToken,
-			{
-        sameSite:'lax',
-				httpOnly: true,
-        path : '/',
-				maxAge: 24 * 60 * 60 * 1000
-			}
-		);
-    // response.redirect('http://10.19.247.186:3000/oauth/test');
-    // response.redirect('http://10.19.234.199:3001/' + '?accessToken=' + (await jwtToken).accessToken + '&refreshToken=' + (await jwtToken).refreshToken);
-    response.redirect('http://10.19.247.186:3001' + '?token=' + (await jwtToken).accessToken, 302);
-    console.log("++++토큰", (await jwtToken).accessToken);
-    console.log("++++토큰", (await jwtToken).refreshToken);
 
-    return (response)
-	}
+    const jwtToken = await this.login(user.intra_id);
+    response.cookie('accessToken', jwtToken.accessToken, {
+      sameSite: 'lax',
+      httpOnly: true,
+      path: '/',
+      maxAge: 24 * 60 * 60 * 1000,
+    });
+    response.cookie('refreshToken', jwtToken.refreshToken, {
+      sameSite: 'lax',
+      httpOnly: true,
+      path: '/',
+      maxAge: 24 * 60 * 60 * 1000,
+    });
+    response.redirect(
+      `http://${this.config.get('FRONT_HOST')}:${this.config.get(
+        'FRONT_PORT',
+      )}?token=${jwtToken.accessToken}`,
+      302,
+    );
+    return response;
+  }
 
-	async getUserNickByToken(token: string): Promise<string> {
-		const user = await this.authJwtService.jwtVerify(token);
-    console.log("+++getUserNickByToken", user);
-		const userFind = await this.userRepository.findOneBy({intra_id : user.intra_id});
+  async getUserNickByToken(token: string): Promise<string> {
+    const user = await this.authJwtService.jwtVerify(token);
+    // console.log('+++getUserNickByToken', user);
+    const userFind = await this.userRepository.findOneBy({
+      intra_id: user.intra_id,
+    });
     if (!userFind) {
       return undefined;
     }
-    console.log('.nickname: ', userFind.nickname);
-		return userFind.nickname;
-	}
-
+    return userFind.nickname;
+  }
 
   async sendEmail(id: string, email: string): Promise<void> {
     if (hashedCodes[id]) return;
@@ -201,17 +183,17 @@ export class AuthService {
     return resDto;
   }
 
-	async login(intra_id: string) {
-		const accessToken = await this.authJwtService.createAccessJwt(intra_id);
-		const refreshToken = await this.authJwtService.createRefreshJwt(intra_id);
+  async login(intra_id: string) {
+    const accessToken = await this.authJwtService.createAccessJwt(intra_id);
+    const refreshToken = await this.authJwtService.createRefreshJwt(intra_id);
 
-		return ({
-			accessToken: accessToken,
-			refreshToken: refreshToken
-		})
-	}
+    return {
+      accessToken: accessToken,
+      refreshToken: refreshToken,
+    };
+  }
 
-	async logout() {
-		//소켓 연결 끊기 
-	}
+  async logout() {
+    //소켓 연결 끊기
+  }
 }

16b1d55c3bf56d83e67d1f3606b3b5da7f97cffc
yongwkim <kyw9404@gmail.com>
2022-08-11 09:38:07 +0900

    [Fix - Back] 닉네임 validation 수정

--- back/server/src/users/dto/create-user.dto.ts
+++ back/server/src/users/dto/create-user.dto.ts
@@ -1,13 +1,14 @@
-import { IsEmail, Length } from 'class-validator';
+import { IsAlphanumeric, IsEmail, Length } from 'class-validator';
 import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateUserDto {
   @ApiProperty({ description: '유저 인트라 아이디' })
-  @Length(1, 10)
+  @Length(1, 8)
   intraId: string;
 
   @ApiProperty({ description: '유저 닉네임' })
-  @Length(1, 10)
+  @Length(1, 8)
+  @IsAlphanumeric()
   nickName: string;
 
   @ApiProperty({ description: '유저 인트라 이메일' })

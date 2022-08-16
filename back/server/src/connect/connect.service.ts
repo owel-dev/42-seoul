df933095863f52723ef65964653c6b85a0c7052a
yongwkim <kyw9404@gmail.com>
2022-08-16 09:38:25 +0900

    [Fix - Back] 소켓 disconnect 시 나가기 에러 수정

--- back/server/src/connect/connect.service.ts
+++ back/server/src/connect/connect.service.ts
@@ -11,70 +11,69 @@ import { Repository } from 'typeorm';
 
 @Injectable()
 export class ConnectService {
-    constructor(
-        private readonly authService: AuthService,
-        private readonly chatService: ChatService,
-        private readonly gameService: GameService,
-        @InjectRepository(User)
-        private readonly userRepository: Repository<User>,
-        private readonly gameManager: GameManager,
-    ) { }
+  constructor(
+    private readonly authService: AuthService,
+    private readonly chatService: ChatService,
+    private readonly gameService: GameService,
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
+    private readonly gameManager: GameManager,
+  ) {}
 
-    async handleConnection(socket: Socket, server: Server) {
-        console.log(`New client connected: ${socket.id}`);
+  async handleConnection(socket: Socket, server: Server) {
+    console.log(`New client connected: ${socket.id}`);
 
-        // 소켓 연결한 유저 닉네임 가져오기.
-        const token = socket.handshake.query.accessToken as string;
-        const clientNick = await this.authService.getUserNickByToken(token);
-        if (!clientNick) return;
+    // 소켓 연결한 유저 닉네임 가져오기.
+    const token = socket.handshake.query.accessToken as string;
+    const clientNick = await this.authService.getUserNickByToken(token);
+    if (!clientNick) return;
 
-        console.log(`nick: ${clientNick}, socket: ${socket.id}`);
+    console.log(`nick: ${clientNick}, socket: ${socket.id}`);
 
-        // 닉네임으로 row 가져오기.
-        const client = await this.userRepository.findOneBy({
-            nickname: clientNick,
-        });
+    // 닉네임으로 row 가져오기.
+    const client = await this.userRepository.findOneBy({
+      nickname: clientNick,
+    });
 
-        ChatService.users.push(new ChatUser(client.intra_id, socket));
+    ChatService.users.push(new ChatUser(client.intra_id, socket));
 
-        // socket_id 세팅
-        client.socket_id = socket.id;
-        // status 세팅
-        if (client.status == 'spectate') socket.join(client.channel_id);
-        else if (client.status == 'gaming') {
-            this.gameManager.changeSocket(client.channel_id, clientNick, socket);
-        }
-        else if (client.status == 'offline') {
-            client.status = 'online';
-            client.channel_id = '0';
-        }
-        await this.userRepository.save(client);
-
-        // chat user에 추가
-        socket.join(client.intra_id);
-        this.chatService.joinChannel(socket, { channelId: '0' }, server);
-        socket.emit('connected');
+    // socket_id 세팅
+    client.socket_id = socket.id;
+    // status 세팅
+    if (client.status == 'spectate') socket.join(client.channel_id);
+    else if (client.status == 'gaming') {
+      this.gameManager.changeSocket(client.channel_id, clientNick, socket);
+    } else if (client.status == 'offline') {
+      client.status = 'online';
+      client.channel_id = '0';
     }
+    await this.userRepository.save(client);
 
-    async handleDisconnect(socket: Socket, server: Server) {
-        console.log(`Chat: client disconnected: ${socket.id}`);
-        const user = ChatService.users.find((user) => user.socket.id === socket.id);
-        if (!user) {
-            return;
-        }
-        const findUser = await this.userRepository.findOneBy({
-            intra_id: user.intraId,
-        });
+    // chat user에 추가
+    socket.join(client.intra_id);
+    this.chatService.joinChannel(socket, { channelId: '0' }, server);
+    socket.emit('connected');
+  }
 
-        this.chatService.leaveChannel(socket, server);
-        if (findUser.channel_id !== '0')
-            this.gameService.clientLeave(socket, server);
-
-        ChatService.users = ChatService.users.filter(
-            (user) => user.socket.id !== socket.id,
-        );
-
-        findUser.status = 'offline';
-        this.userRepository.save(findUser);
+  async handleDisconnect(socket: Socket, server: Server) {
+    console.log(`Chat: client disconnected: ${socket.id}`);
+    const user = ChatService.users.find((user) => user.socket.id === socket.id);
+    if (!user) {
+      return;
     }
+    const findUser = await this.userRepository.findOneBy({
+      intra_id: user.intraId,
+    });
+
+    this.chatService.leaveChannel(socket, server);
+    if (findUser.channel_id !== '0')
+      await this.gameService.clientLeave(socket, server);
+
+    ChatService.users = ChatService.users.filter(
+      (user) => user.socket.id !== socket.id,
+    );
+
+    findUser.status = 'offline';
+    this.userRepository.save(findUser);
+  }
 }

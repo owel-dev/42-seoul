bdceb939f85f2a3ad05e5b4781343dd4910fdfa6
yongwkim <kyw9404@gmail.com>
2022-08-24 10:47:00 +0900

    [Fix - Back] 서버 재실행 시 모든 유저 status, channelId 초기화

--- back/server/src/main.ts
+++ back/server/src/main.ts
@@ -4,29 +4,31 @@ import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { UsersService } from './users/users.service';
 
-
-var cookieParser = require('cookie-parser')
+var cookieParser = require('cookie-parser');
 
 async function bootstrap() {
-	const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
-	const config = new DocumentBuilder()
-		.setTitle('Hi-transcendence')
-		.setDescription(`transcendence API document description`)
-		.setVersion('1.0')
-		.build();
-	const document = SwaggerModule.createDocument(app, config);
-	SwaggerModule.setup('api', app, document);
+  const config = new DocumentBuilder()
+    .setTitle('Hi-transcendence')
+    .setDescription(`transcendence API document description`)
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
-	app.useStaticAssets(join(__dirname, '../dist/uploads'), {
-		prefix: '/public',
-	});
-	app.use(cookieParser());
-	app.enableCors();
-	app.useGlobalPipes(new ValidationPipe());
+  app.useStaticAssets(join(__dirname, '../dist/uploads'), {
+    prefix: '/public',
+  });
+  app.use(cookieParser());
+  app.enableCors();
+  app.useGlobalPipes(new ValidationPipe());
 
+  const userService = app.get(UsersService);
+  userService.initializeStatus();
 
-	await app.listen(3000);
+  await app.listen(3000);
 }
 bootstrap();
